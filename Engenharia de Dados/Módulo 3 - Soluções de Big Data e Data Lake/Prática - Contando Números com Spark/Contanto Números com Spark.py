ef90fd5684778dd4189b86bbbcd5eead71f3849e
Lucas Martins <lucasvm20@hotmail.com>
2025-11-02 16:30:09 -0300

    Atualização de link no arquivo .py
    Atualização no link do arquivo .py para quem abrir o link visualizá-lo como leitor

--- Engenharia de Dados/Módulo 3 - Soluções de Big Data e Data Lake/Prática - Contando Números com Spark/Contanto Números com Spark.py
+++ Engenharia de Dados/Módulo 3 - Soluções de Big Data e Data Lake/Prática - Contando Números com Spark/Contanto Números com Spark.py
@@ -1,6 +1,6 @@
 # Contando Números com Spark
 # Contando quantas vezes os números (de 1-10) aparecerão no arquivo numbers.txt
-# Link para o download no arquivo: https://drive.google.com/open?id=10T-DiRYf-uF5JHcMhpOOFjj_msYLanJ1&usp=drive_fs
+# Link para o download no arquivo: https://drive.google.com/file/d/10T-DiRYf-uF5JHcMhpOOFjj_msYLanJ1/view?usp=sharing
 
 # %%
 # Importando Biblioteca Spark
