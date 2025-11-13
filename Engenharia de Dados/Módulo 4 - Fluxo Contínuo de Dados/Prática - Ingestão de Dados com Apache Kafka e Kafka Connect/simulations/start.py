c30781eab0123300ff20098a98082fe0637fa6ae
Lucas Martins <lucasvm20@hotmail.com>
2025-11-13 02:39:43 -0300

    Prática de Ingestão de Dados com Kafka e Kafka Connect

---
+++ Engenharia de Dados/Módulo 4 - Fluxo Contínuo de Dados/Prática - Ingestão de Dados com Apache Kafka e Kafka Connect/simulations/start.py
@@ -0,0 +1,11 @@
+import subprocess
+import sys
+
+# Instala as dependências
+subprocess.check_call([sys.executable, "-m", "pip", "install", 
+                       "--break-system-packages", "-r", 
+                       "./simulations/requirements.txt"])
+
+
+# Executa o programa principal (substitua 'main.py' pelo seu script)
+# subprocess.run([sys.executable, "main.py"])
